afc8677a04eadaa729248ba8315fe849543b822f
Juan Pablo <juanpanime@gmail.com>
2022-05-19 23:38:43 -0500

    Change invited table.

--- src/Users/Invited.jsx
+++ src/Users/Invited.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { NavLink} from 'react-router-dom';
+import MaterialTable from "material-table";
 import SpacingGrid from '../Components/AvailabilityPie';
-import Table from '../Components/Table'
 import AuthService from '../Services/AuthService';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -9,51 +9,24 @@ import axios from "axios";
 
 export default function Invited() {
   const [tax, setTax] = useState([]);
-  const [regIn, setRegIn] = useState([]);
-  const [regOut, setRegOut] = useState([]);
-
   const endTarifas = "tarifasCompuestas";
-  const endOut = "registros/false";
-  const endInput = "registros/true";
-
-  const colRegistros =[
-      { field: 'id_registro', headerName: 'ID', hide: true},  
-      { field: 'placa', headerName: 'Placa', editable: true},
-      { field: 'nombre_vehiculo', headerName: 'Tipo', editable: true},
-      { field: 'ingreso', headerName: 'Hora Ingreso', editable: true},
-      { field: 'salida', headerName: 'Hora Salida', editable: true},
-      { field: 'total_horas', headerName: 'Total Tiempo', editable: true},
-      { field: 'pago_inicial', headerName: 'Pago Inicial', editable: true},
-      { field: 'pago_final', headerName: 'Pago Final', editable: true},
-      { field: 'total_costo', headerName: 'Total Costo', editable: true},
-      { field: 'nombre', headerName: 'Realizo', editable: true},
-      { field: 'tipo_tarifa', headerName: 'Tipo Tarifa', editable: true}
-  ]
-  
   const colTarifas = [
-      { field: 'id_vehiculo', headerName: 'ID', hide: true },  
-      { field: 'nombre_vehiculo', headerName: 'Tipo', width: 200, editable: true},
-      { field: 'cuarto_hora', headerName: 'Cuarto de hora',  width: 200, editable: true},
-      { field: 'hora', headerName: 'Hora',  width: 200, editable: true},
-      { field: 'seis_horas', headerName: 'Medio dia', width: 200, editable: true},
-      { field: 'dia', headerName: 'Dia', width: 200, editable: true},
-      { field: 'mes', headerName: 'Mes', width: 200, editable: true}
+    { field: 'id_vehiculo', title: 'ID', hide: true },  
+    { field: 'nombre_vehiculo', title: 'Tipo'},
+    { field: 'cuarto_hora', title: 'Cuarto de hora'},
+    { field: 'hora', title: 'Hora',  width: 200 },
+    { field: 'seis_horas', title: 'Medio dia'},
+    { field: 'dia', title: 'Dia'},
+    { field: 'mes', title: 'Mes'}
   ];
 
   useEffect(() => {
     const dataresp = async () =>{      
       const responseTax = await axios.get(AuthService.API_URL + endTarifas )
-      const responseRegIn = await axios.get(AuthService.API_URL + endInput )
-      const responseRegOut = await axios.get(AuthService.API_URL + endOut )
       setTax(responseTax.data)
-      setRegIn(responseRegIn.data)
-      setRegOut(responseRegOut.data)
     }
     dataresp()
   }, []);
-
-  const idReg = (row) => row.id_registro
-  const idTax = (row) => row.id_vehiculo
 
 return (
 <Box sx={{ flexGrow: 1 }}>
@@ -69,8 +42,17 @@ return (
       <h1>Bienvenidos a ParkingApp</h1>
     </Grid>
 
-    <Grid item xs={12} align="center" style={{ height: 215, width: "100%" }}>
-      <Table data={tax} id={idTax} col={colTarifas} />
+    <Grid item xs={12} align="center" style={{ height: 215, width: "90%" }}>
+      <MaterialTable
+          columns={colTarifas}
+          data={tax}
+          title="Tarifas"
+          options={{
+            paging: false,
+            search: false,
+            sorting: false
+          }}
+        />
     </Grid>
 
     <Grid item xs={12} container style={{ width: "90%" }}>
